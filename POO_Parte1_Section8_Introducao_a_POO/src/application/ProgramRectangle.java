8e8b8d5a0ec61b7c55e31797230ee7276ab73653
salveodojuliao <batistajulio215@gmail.com>
2023-05-12 10:46:08 -0300

    update: add new java project of exericses - section 8 - introducao a POO

---
+++ POO_Parte1_Section8_Introducao_a_POO/src/application/ProgramRectangle.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Rectangle;
+
+
+public class ProgramRectangle {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Rectangle rectangleX = new Rectangle();
+		
+		System.out.println("Enter rectangle width and height");
+		rectangleX.width = sc.nextDouble();
+		rectangleX.height = sc.nextDouble();
+		
+		System.out.println(rectangleX);
+		sc.close();
+	}
+}

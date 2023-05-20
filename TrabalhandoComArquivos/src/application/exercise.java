baf2add237f148e4809896e2b13490ad5ac3ed3d
JayCesar <batistajulio215@gmail.com>
2023-05-20 13:21:37 -0300

    update: add my solution to the exercise - with research

--- TrabalhandoComArquivos/src/application/exercise.java
+++ TrabalhandoComArquivos/src/application/exercise.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -14,56 +13,57 @@ import java.util.Scanner;
 
 import entities.Product;
 
-public class Test {
-	public static void main(String[] args) throws ParseException {
-
+public class exercise {
+	
+	public static void main (String[] args) {
+		// D:\\workspace\\ws-eclipse\\java_exercises\\TrabalhandoComArquivos\\files_ex\\input.csv
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-
-		List<Product> list = new ArrayList<>();
-
-		System.out.println("Enter file path:");
-		String sourceFileStr = sc.nextLine();
-
-		File sourceFile = new File(sourceFileStr);
-		String sourceFolderStr = sourceFile.getParent();
 		
+		List<Product> list = new ArrayList<>();
+		
+		System.out.println("Enter file path: ");
+		String sourceFileStr = sc.nextLine();
+		
+		File sourceFile = new File(sourceFileStr);
+		
+		String sourceFolderStr = sourceFile.getParent(); // Despreza o nome do arquivo - pega só o caminho
+		
+		// Criando uma pasta
 		boolean success = new File(sourceFolderStr + "\\out").mkdir();
 		
 		String targetFileStr = sourceFolderStr + "\\out\\summary.csv";
-
-		try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))) {
-
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))){
+			
 			String itemCsv = br.readLine();
 			while (itemCsv != null) {
-
 				String[] fields = itemCsv.split(",");
 				String name = fields[0];
 				double price = Double.parseDouble(fields[1]);
 				int quantity = Integer.parseInt(fields[2]);
-
+				
 				list.add(new Product(name, price, quantity));
-
 				itemCsv = br.readLine();
 			}
-
-			try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
-
+			
+			// Agora vou escrever no arquivo
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))){
 				for (Product item : list) {
 					bw.write(item.getName() + "," + String.format("%.2f", item.total()));
 					bw.newLine();
 				}
-
-				System.out.println(targetFileStr + " CREATED!");
-				
-			} catch (IOException e) {
+			}
+			catch(IOException e) {
 				System.out.println("Error writing file: " + e.getMessage());
 			}
-
-		} catch (IOException e) {
-			System.out.println("Error reading file: " + e.getMessage());
+			
 		}
-
+		catch(IOException e) {
+			System.out.println("Error writing file: " + e.getMessage());
+		}
+			
 		sc.close();
 	}
+	
 }
